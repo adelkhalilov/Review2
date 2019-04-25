2ac3d0ac6f61bbf32fc564703a0a86d2bc3df6ed
adelkhalilov <45914951+adelkhalilov@users.noreply.github.com>
2019-04-25 21:38:48 +0300

    Update client.py

--- client.py
+++ client.py
@@ -1,7 +1,7 @@
 import argparse
 import requests
 
-adres = '127.0.0.1'
+address = '127.0.0.1'
 PORT = '5000'
 
 
@@ -10,40 +10,57 @@ def get_args():
     subject_name = None
     day_of_the_week = None
     time_of_start = None
+    current_day = None
+    current_time = None
     parser = argparse.ArgumentParser()
     subs = parser.add_subparsers(dest='action')
-    add = subs.add_parser('add', help='Add')
+    add = subs.add_parser('add_lesson', help='Add_lesson')
     add.add_argument('--subject_name', help='subject_name')
     add.add_argument('--day_of_the_week', dest='day_of_the_week', help='Day_of_the_week')
     add.add_argument('--time_of_start', dest='time_of_start', help='Time_of_start')
-    get = subs.add_parser('get', help='Get')
+    get_lessons = subs.add_parser('get_lessons', help='Get_lessons')
+    get_next_lesson = subs.add_parser('get_next_lesson', help='Get_next_lesson')
+    get_next_lesson.add_argument('--current_day', dest='current_day', help='Current_day')
+    get_next_lesson.add_argument('--current_time', dest='current_time', help='Current_time')
     args = parser.parse_args()
-    if args.action == 'add':
+    if args.action == 'add_lesson':
         action = args.action
         subject_name = args.subject_name
         day_of_the_week = args.day_of_the_week
         time_of_start = args.time_of_start
-    if args.action == 'get':
+    if args.action == 'get_lessons':
         action = args.action
-    return action, subject_name, day_of_the_week, time_of_start
+    if args.action == 'get_next_lesson':
+        action = args.action
+        current_day = args.current_day
+        current_time = args.current_time
+    return action, subject_name, day_of_the_week, time_of_start, current_time, current_day
 
 def add_lesson(subject_name, day_of_the_week, time_of_start):
-    r = requests.post('http://' + adres + ':' + PORT + '/add_lesson', params={'subject_name': subject_name, 'day_of_the_week': day_of_the_week, 'time_of_start': time_of_start})
+    r = requests.post('http://' + address + ':' + PORT + '/add_lesson', params={'subject_name': subject_name, 'day_of_the_week': day_of_the_week, 'time_of_start': time_of_start})
     return (r.text)
 
 
 def get_lessons():
-    r = requests.post('http://' + adres + ':' + PORT + '/get_lessons')
+    r = requests.get('http://' + address + ':' + PORT + '/get_lessons')
+    return (r.text)
+
+def get_next_lesson(current_day, current_time):
+    r = requests.get('http://' + address + ':' + PORT + '/get_next_lesson', params={'current_day': current_day, 'current_time': current_time})
     return (r.text)
 
 def main():
     action = ''; subject_name = ''; day_of_the_week = ''; time_of_start = ''
+    current_day = ''; current_time = ''
     args = get_args()
     action = args[0]; subject_name = args[1]; day_of_the_week = args[2]; time_of_start = args[3];
-    if action == 'add':
+    current_day = args[4]; current_time = args[5]
+    if action == 'add_lesson':
         print(add_lesson(subject_name, day_of_the_week, time_of_start))
-    if action == 'get':
+    if action == 'get_lessons':
         print(get_lessons())
+    if action == 'get_next_lesson':
+        print(get_next_lesson(current_day, current_time))
 
 if __name__ == '__main__':
     main()
