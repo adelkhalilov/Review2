0155bbeef3a47baf288e310e38a615c7e7685739
adelkhalilov <45914951+adelkhalilov@users.noreply.github.com>
2019-05-20 01:17:17 +0300

    Update config.py

--- config.py
+++ config.py
@@ -2,7 +2,6 @@ from datetime import datetime
 
 params = dict(dbname="test", user="postgres", password="1234", host="localhost")
 day = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+numday = {'Monday': 0, 'Tuesday': 1, 'Wednesday': 2, 'Thursday': 3, 'Friday': 4, 'Saturday': 5, 'Sunday': 6}
 PORT = '5000'
 address = '127.0.0.1'
-
-
