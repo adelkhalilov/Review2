705a469b50327ede2ab268df66f5bb88ba8ca58b
adelkhalilov <45914951+adelkhalilov@users.noreply.github.com>
2019-05-19 20:29:32 +0300

    Add files via upload

---
+++ config.py
@@ -0,0 +1,8 @@
+from datetime import datetime
+
+params = dict(dbname="test", user="postgres", password="1234", host="localhost")
+day = ("Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday", "Sunday")
+PORT = '5000'
+address = '127.0.0.1'
+
+
